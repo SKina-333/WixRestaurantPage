3b596aac4d99beada9b92bd64f162eae3ba54f33
Silvar <kinaho2013@gmail.com>
2024-08-19 01:05:21 +1000

    added tab switching

--- src/index.js
+++ src/index.js
@@ -2,4 +2,22 @@ import "./style.css";
 import loadFontAwesome from './javascript/icons.js';
 
 loadFontAwesome();
-console.log("Hello World!");
+
+
+const tab_swtiches = document.querySelectorAll(".tab_switch");
+const content_page = document.querySelectorAll(".content");
+
+tab_swtiches.forEach((tab,index) => {
+
+    tab.addEventListener("click", (e) => {
+        tab_swtiches.forEach((tab) => {
+            tab.classList.remove("active");
+        });
+        tab.classList.add("active");
+
+        content_page.forEach((content) => {
+            content.classList.add("hidden");
+        });
+        content_page[index].classList.remove("hidden");
+    });
+});
